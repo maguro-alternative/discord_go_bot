ae2f9f092c97d12ec2b721f3e180ed62ab826f5b
ygo <ka.ra.si.naporitan@gmail.com>
2023-10-07 10:33:41 +0900

    データベース関連の環境変数も削除

--- model/envconfig/env.go
+++ model/envconfig/env.go
@@ -8,13 +8,6 @@ import (
 
 type Env struct {
 	TOKEN            string
-	DatabaseType	 string
-	DatabaseURL      string
-	DatabaseName     string
-	DatabaseUser     string
-	DatabasePassword string
-	DatabaseHost     string
-	DatabasePort     string
 	ServerPort       string
 }
 
@@ -26,13 +19,6 @@ func NewEnv() (*Env, error) {
 
 	return &Env{
 		TOKEN:            os.Getenv("TOKEN"),
-		DatabaseType:	  "postgresql",
-		DatabaseURL:      os.Getenv("PGURL"),
-		DatabaseName:     os.Getenv("PGDATABASE"),
-		DatabaseUser:     os.Getenv("PGUSER"),
-		DatabasePassword: os.Getenv("PGPASSWORD"),
-		DatabaseHost:     os.Getenv("PGHOST"),
-		DatabasePort:     os.Getenv("PGPORT"),
 		ServerPort:       os.Getenv("PORT"),
 	}, nil
 }
